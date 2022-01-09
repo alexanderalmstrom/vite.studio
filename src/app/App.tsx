b7b85e1188d3cc5f7bbdf5a5329db574311e7177
Alexander Almström <alexander.almstrom@madepeople.se>
2022-01-09 04:45:27 +0100

    Testing react experimental and suspense
    Move helmet provider to app
    
    Fix build errors with legacy peer deps
    
    Test allow legacy peer deps
    
    Test build

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,23 @@
+import React, { Suspense } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
+import Loading from './components/Loading';
+
+const Layout = React.lazy(() => import('./components/Layout'));
+
 import './App.scss';
-import Layout from './components/Layout';
 
 export default function App() {
   return (
-    <Layout>
-      <div className="h-screen flex flex-col justify-center items-center">
-        <h1 className="text-4xl">vite.studio</h1>
-      </div>
-    </Layout>
+    <React.StrictMode>
+      <HelmetProvider>
+        <Suspense fallback={<Loading />}>
+          <Layout>
+            <div className="app h-screen flex flex-col justify-center items-center">
+              <h1 className="text-4xl">vite.studio</h1>
+            </div>
+          </Layout>
+        </Suspense>
+      </HelmetProvider>
+    </React.StrictMode>
   );
 }

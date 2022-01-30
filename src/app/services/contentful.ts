794a97735a93cecd5e6d9ef2219d67c0806d8999
Alexander Almström <alexander.almstrom@madepeople.se>
2022-01-30 23:07:02 +0100

    Come on

--- src/app/services/contentful.ts
+++ src/app/services/contentful.ts
@@ -3,18 +3,18 @@ import { ContentfulClientApi, createClient } from 'contentful';
 let space;
 let accessToken;
 
-if (process.env.NODE_ENV === 'production') {
-  if (import.meta.env.SSR) {
-    space = process.env.CONTENTFUL_SPACE_ID as string;
-    accessToken = process.env.CONTENTFUL_ACCESS_TOKEN as string;
-  } else {
-    space = process.env.VITE_CONTENTFUL_SPACE_ID as string;
-    accessToken = process.env.VITE_CONTENTFUL_ACCESS_TOKEN as string;
-  }
-} else {
-  space = import.meta.env.VITE_CONTENTFUL_SPACE_ID as string;
-  accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN as string;
-}
+// if (import.meta.env.SSR) {
+//   space = import.meta.env.VITE_CONTENTFUL_SPACE_ID as string;
+//   accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN as string;
+// } else {
+//   space = process.env.VITE_CONTENTFUL_SPACE_ID as string;
+//   accessToken = process.env.VITE_CONTENTFUL_ACCESS_TOKEN as string;
+// }
+
+space = (import.meta.env.VITE_CONTENTFUL_SPACE_ID ||
+  process.env.VITE_CONTENTFUL_SPACE_ID) as string;
+accessToken = (import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN ||
+  process.env.VITE_CONTENTFUL_ACCESS_TOKEN) as string;
 
 export const contentfulClient: ContentfulClientApi = createClient({
   space,

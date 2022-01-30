6b39cce450195ec9475de8f3989680a6b8d6a2e7
Alexander Almström <alexander.almstrom@madepeople.se>
2022-01-30 20:35:21 +0100

    Update env names

--- src/app/services/contentful.ts
+++ src/app/services/contentful.ts
@@ -1,10 +1,10 @@
 import { ContentfulClientApi, createClient } from 'contentful';
 
 const space = (import.meta.env.VITE_CONTENTFUL_SPACE_ID ||
-  process.env.VITE_CONTENTFUL_SPACE_ID) as string;
+  process.env.CONTENTFUL_SPACE_ID) as string;
 
 const accessToken = (import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN ||
-  process.env.VITE_CONTENTFUL_ACCESS_TOKEN) as string;
+  process.env.CONTENTFUL_ACCESS_TOKEN) as string;
 
 export const contentfulClient: ContentfulClientApi = createClient({
   space,

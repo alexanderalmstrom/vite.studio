1f4dd4db880709d7421f640540205b1f9a863e9e
Alexander Almström <alexander.almstrom@madepeople.se>
2022-01-30 22:31:55 +0100

    This might work

--- src/app/services/contentful.ts
+++ src/app/services/contentful.ts
@@ -4,15 +4,16 @@ let space;
 let accessToken;
 
 if (process.env.NODE_ENV === 'production') {
-  space = (process.env.VITE_CONTENTFUL_SPACE_ID ||
-    process.env.CONTENTFUL_SPACE_ID) as string;
-  accessToken = (process.env.VITE_CONTENTFUL_ACCESS_TOKEN ||
-    process.env.CONTENTFUL_ACCESS_TOKEN) as string;
+  if (import.meta.env.SSR) {
+    space = process.env.CONTENTFUL_SPACE_ID as string;
+    accessToken = process.env.CONTENTFUL_ACCESS_TOKEN as string;
+  } else {
+    space = process.env.VITE_CONTENTFUL_SPACE_ID as string;
+    accessToken = process.env.VITE_CONTENTFUL_ACCESS_TOKEN as string;
+  }
 } else {
-  space = (import.meta.env.VITE_CONTENTFUL_SPACE_ID ||
-    import.meta.env.CONTENTFUL_SPACE_ID) as string;
-  accessToken = (import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN ||
-    import.meta.env.CONTENTFUL_ACCESS_TOKEN) as string;
+  space = import.meta.env.VITE_CONTENTFUL_SPACE_ID as string;
+  accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN as string;
 }
 
 export const contentfulClient: ContentfulClientApi = createClient({

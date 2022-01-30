f757ffcbc54bd3bfa77258e715f08372f6edb36b
Alexander Almström <alexander.almstrom@madepeople.se>
2022-01-30 17:24:00 +0100

    Use process env in contentful client

--- src/app/services/contentful.ts
+++ src/app/services/contentful.ts
@@ -1,7 +1,9 @@
 import { createClient } from 'contentful';
 
-const SPACE_ID = import.meta.env.VITE_CONTENTFUL_SPACE_ID as string;
-const ACCESS_TOKEN = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN as string;
+const SPACE_ID = (import.meta.env.VITE_CONTENTFUL_SPACE_ID ||
+  process.env.VITE_CONTENTFUL_SPACE_ID) as string;
+const ACCESS_TOKEN = (import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN ||
+  process.env.VITE_CONTENTFUL_ACCESS_TOKEN) as string;
 
 export const contentfulClient = createClient({
   space: SPACE_ID,

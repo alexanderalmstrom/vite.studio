49089170499ce051207b4899ce76ff08aac46228
Alexander Almström <alexander.almstrom@madepeople.se>
2022-01-30 23:38:02 +0100

    Test

--- src/app/services/contentful.ts
+++ src/app/services/contentful.ts
@@ -1,7 +1,9 @@
 import { ContentfulClientApi, createClient } from 'contentful';
 
-const space = import.meta.env.VITE_CONTENTFUL_SPACE_ID as string;
-const accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN as string;
+const space = (import.meta.env.VITE_CONTENTFUL_SPACE_ID ||
+  process.env.VITE_CONTENTFUL_SPACE_ID) as string;
+const accessToken = (import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN ||
+  process.env.VITE_CONTENTFUL_ACCESS_TOKEN) as string;
 
 export const contentfulClient: ContentfulClientApi = createClient({
   space,
